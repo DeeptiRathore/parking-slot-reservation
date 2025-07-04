99983f08359ab3b6d9e9f49a515c63ad8c7395f7
HP <HP@DESKTOP-VUS8L0H>
2025-07-04 23:57:37 +0530

    This is my first cmmit

---
+++ ParkingSlotReservationSystem/src/main/java/com/parking/ParkingSlotReservationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.parking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingSlotReservationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingSlotReservationSystemApplication.class, args);
+	}
+
+}
